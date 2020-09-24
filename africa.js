7ea1f0d201f22f4b2acd9ec0398178249d04e435
javasrcript-dev <makhosindondo@outlook.com>
2020-09-24 21:55:55 +0200

    Thursday 09/24/2020 21:55 - hide the .env

--- africa.js
+++ africa.js
@@ -1,4 +1,3 @@
-const cron = require("node-cron");
 const puppeteer = require("puppeteer");
 require('dotenv').config();
 const wsChromeEndpointurl = require('../browser');
@@ -6,14 +5,11 @@ const vars = require('../store/storeVars');
 const express = require("express");
 const Routa = express.Router();
 ///
-process.setMaxListeners(Infinity);
-let src_name = "Africanews";
+// process.setMaxListeners(Infinity);
 //
-let news = [];
-let trending = [];
+let data = [];
 async function main(uri) {
     try {
-        let url_src = uri;
         const browser = await puppeteer.connect({
             browserWSEndpoint: wsChromeEndpointurl,
         });
@@ -22,9 +18,9 @@ async function main(uri) {
         await page.goto(uri, { waitUntil: 'networkidle2', timeout: 0 });
         await page.waitFor(125000);
         await page.waitForSelector('article');
-        const emAll = await page.$$('article.just-in__article');
+        const items = await page.$$('article.just-in__article');
 
-        for (const each of emAll) {
+        for (const item of items) {
             try {
                 const time = await each.$('time');
                 const ab = await each.$('a');
@@ -33,78 +29,17 @@ async function main(uri) {
                 const headline = await each.$eval('h3 > a', a => a.innerText);
                 //
                 const link = await page.evaluate(a => a.href, ab);
-                trending.push({
-                    url_src,
-                    src_name,
-                    "date": date.replace(/(\r\n|\n|\r)/gm, "").trim(),
-                    "url": link,
-                    "headline": headline.replace(/(\r\n|\n|\r)/gm, "").trim(),
-                })
+                data.push({
+                    /* 
+                    
+                    
+                    */
+  
+                  })
             } catch (error) {
                 console.log('\x1b[42m%s\x1b[0m', `From ${uri} loop: ${error.name}`)
 
             }
-        }
-
-
-        //
-        const items = await page.$$('article');
-        //
-
-        for (const item of items) {
-            try {
-                //
-                const timeStamp = await item.$('.boxPlay--duration');
-                const e = await item.$('img');
-                const f = await item.$('.teaser__title');
-                const time = await item.$('time');
-                //
-                const url = await page.evaluate(a => a.href, item);
-                const head = await item.$eval('.teaser__title', a => a.textContent);
-                const date = (time != null || undefined) ? await page.evaluate(i => i.textContent, time).trim() : null;
-                const thumbnail = (e != null || undefined) ? await item.$eval('img', img => img.src) : null;
-                const vidLen = (timeStamp != null || undefined) ? await page.evaluate(a => a.innerText, timeStamp) : null;
-                const isVid = (timeStamp != null || undefined) ? true : false;
-                //
-                let headline = head.trim();
-                const iHtml = await page.evaluate(el => el.innerHTML, item);
-                let empty = null;
-                let emptyArr = "";
-                let src = "https://www.screenafrica.com/wp-content/uploads/2018/04/Africanews-logo.png";
-                let lede = empty;
-                let author = empty;
-                let tag = empty;
-                let category = empty;
-                let catLink = empty;
-                let images = emptyArr;
-
-
-                news.push({
-                    url_src,
-                    src_name,
-                    url,
-                    headline,
-                    lede,
-                    thumbnail,
-                    src,
-                    //
-                    category,
-                    catLink,
-                    tag,
-                    //
-                    images,
-                    //
-                    isVid,
-                    vidLen,
-                    //
-                    author,
-                    date
-                })
-            } catch (error) {
-                console.log('\x1b[42m%s\x1b[0m', `From ${uri} loop: ${error.name}`)
-                continue;
-            }
-
         }
         //
         await page.close();
@@ -116,17 +51,7 @@ async function main(uri) {
 }
 let source = "https://www.africanews.com/";
 
-
-
-cron.schedule("0 */6 * * *", () => {
-
-    (() => {
-        console.log('\x1b[46m%s\x1b[0m', "Africanews fired at:" + Date());
-        main(source);
-
-    })();
-});
-/////
+main(source);
 Routa.get('/africa', (req, res) => {
     res.send({
 
